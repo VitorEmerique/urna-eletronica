a8a9458896840e7b8b138e72a7a785771070ae8c
reiscaslureis <reisdossousalucas@gmail.com>
2023-02-05 17:38:11 -0300

    mesario.fxml

--- urna/src/main/java/com/ufopinha/controllers/Urna.java
+++ urna/src/main/java/com/ufopinha/controllers/Urna.java
@@ -1,5 +1,15 @@
 package com.ufopinha.controllers;
 
+import com.ufopinha.daos.EleicaoDAO;
+import com.ufopinha.daos.VotoDAO;
+import com.ufopinha.models.Eleicao;
+
 public class Urna {
+    private VotoDAO votoDao;
+    private EleicaoDAO eleicao;
+
+    public Urna() {
+
+    }
     
 }
---
+++ urna/src/main/java/com/ufopinha/controllers/VotoDao.java
@@ -0,0 +1,7 @@
+package com.ufopinha.controllers;
+
+
+
+public class VotoDao {
+
+}
